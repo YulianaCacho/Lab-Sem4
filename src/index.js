2d4a18ca9c8ffcd404dfe34a5b9a38d2608528d3
YulianaCacho <166524112+YulianaCacho@users.noreply.github.com>
2024-04-16 14:41:49 +0000

    Cambios

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React, {StrictMode } from "react";
 import { createRoot} from "react-dom/client";
 
 
+const Message = ()=>{
+    return <h1>Message: </h1>;
+}
+
 const root = createRoot(document.getElementById("root"));
 root.render(
 <StrictMode>
     <h1>Titulo desde react</h1>
-</StrictMode>
+
+           <Message message="Hola 1"></Message>
+           <Message message="Hola 2"></Message>
+           <Message></Message>
+           <Message message="ayoh 1"></Message>
+
+           </StrictMode>
+
 );
---
+++ src/message.js
@@ -0,0 +1,4 @@
+conts Message = ( { message }) => {
+ return <h1>Message: {message}</h1>;
+}
+export default Message;
